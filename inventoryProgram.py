1b6ca7d03d3238f910c2efedd0f8af7bac25ac42
Lydia Kerns <lkerns94@gmail.com>
2016-06-10 11:11:47 -0700

    figure out sum of dictionary values
    but everything else seems good

--- inventoryProgram.py
+++ inventoryProgram.py
@@ -1,11 +1,12 @@
 #inventory.py
-stuff={'rope':1, 'torch': 6, 'gold coin': 42, 'dagger': 1, 'arrow': 12}
+stuff={'rope': 1, 'torch': 6, 'gold coin': 42, 'dagger': 1, 'arrow': 12}
 
 def displayInventory(inventory):
   print("Inventory:")
   item_total=0
   for k, v in inventory.items():
-    item_total=item_total + v
-    return item_total
-    print('Inventory:')
-    print("Total number of items: " + str(item_total))
+    print(v, k)
+    myList=[]
+    myList.append(v)
+    item_total=sum(myList)
+  print("Total number of items: " + str(item_total))
