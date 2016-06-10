f68a747c7dd6dc93cd2a915601c28d77a891d101
Lydia Kerns <lkerns94@gmail.com>
2016-06-10 11:15:17 -0700

    Fixed!
    myList is declared outside of the for loop, otherwise it will empty the list every time it loops.

--- inventoryProgram.py
+++ inventoryProgram.py
@@ -4,9 +4,9 @@ stuff={'rope': 1, 'torch': 6, 'gold coin': 42, 'dagger': 1, 'arrow': 12}
 def displayInventory(inventory):
   print("Inventory:")
   item_total=0
+  myList=[]
   for k, v in inventory.items():
     print(v, k)
-    myList=[]
     myList.append(v)
     item_total=sum(myList)
   print("Total number of items: " + str(item_total))
