20df7175e96e51e9721124c6356f2414a57fb567
Lydia Kerns <lkerns94@gmail.com>
2016-06-09 17:00:33 -0700

    Update inventoryProgram.py

--- inventoryProgram.py
+++ inventoryProgram.py
@@ -1,13 +1,13 @@
 #inventory.py
 stuff={'rope':1, 'torch': 6, 'gold coin': 42, 'dagger': 1, 'arrow': 12}
 
+stuff={'rope':1, 'torch': 6, 'gold coin': 42, 'dagger': 1, 'arrow': 12}
+
 def displayInventory(inventory):
   print("Inventory:")
   item_total=0
   for k, v in inventory.items():
-    item_total=item_total + v.get(item, 0)
-  return item_total
-print('Inventory:')
-for k, v in inventory.items():
-  print(k + str(v))
-print("Total number of items: " + str(item_total))
+    item_total=item_total + v
+    return item_total
+    print('Inventory:')
+    print("Total number of items: " + str(item_total))
